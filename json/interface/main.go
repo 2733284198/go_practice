ce8fb2dc03d5a9c5aa978cbb34c6bc54712311c1
frank2020 <feixiao2020@sina.com>
2018-07-13 09:37:46 +0800

    json marshal interface

---
+++ json/interface/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+
+// Json Marshal Interface
+
+type Animal interface {
+    Speak() string
+}
+
+type Cat struct{
+	Age int 
+}
+func (c Cat) Speak() string {
+    return "cat"
+}
+
+type Dog struct{
+	Age int 
+}
+
+func (d Dog) Speak() string {
+    return "dog"
+}
+
+
+func main() {
+	m:= make(map[int]Animal, 16)
+	m[1] = &Dog{
+		Age: 1,
+	}
+	buf, err := json.Marshal(m)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	fmt.Println(string(buf))
+}
