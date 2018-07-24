43b514cea44e367327a8c6033e2165bbdc6ad42e
frank2020 <feixiao2020@sina.com>
2018-07-24 18:39:50 +0800

    support cors

--- gin/main.go
+++ gin/main.go
@@ -5,6 +5,8 @@ import (
 	"io"
 	"os"
 	"github.com/feixiao/log4go"
+	"github.com/gin-contrib/cors"
+	"time"
 )
 
 func main() {
@@ -15,6 +17,17 @@ func main() {
 	gin.DefaultWriter = io.MultiWriter(os.Stdout,&GinLogger{})
 
 	r := gin.Default()
+
+	config := cors.Config{
+		AllowAllOrigins: true,
+		AllowCredentials: false,
+		MaxAge: 12 * time.Hour,
+	}
+
+	// "Origin,No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With, userId, token"
+	config.AddAllowHeaders("token","x-access-token","x-url-path")
+
+	r.Use(cors.New(config))
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
